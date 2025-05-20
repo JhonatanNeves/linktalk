ef51169d7aaa483b4906a58ad63097f5b3405ad3
Jhonatan <nevesjn2@gmail.com>
2025-05-20 20:33:47 -0300

    implementation TextFieldFinish

--- app/src/main/java/com/example/linktalk/ui/components/PrimaryTextField.kt
+++ app/src/main/java/com/example/linktalk/ui/components/PrimaryTextField.kt
@@ -1,31 +1,31 @@
 package com.example.linktalk.ui.components
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.linktalk.R
-import com.example.linktalk.ui.theme.ColorError
 import com.example.linktalk.ui.theme.LinkTalkTheme
 
 @Composable
@@ -33,45 +33,77 @@ fun PrimaryTextField(
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier,
+    placeholder: String = "",
     @DrawableRes
     leadingIcon: Int? = null,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
     errorMessage: String? = null,
 ) {
+
+    var passwordVisible by remember {
+        mutableStateOf(false)
+    }
+
+    val isPasswordField = keyboardType == KeyboardType.Password
+    val visualTransformation = when {
+        isPasswordField && !passwordVisible -> PasswordVisualTransformation()
+        else -> VisualTransformation.None
+    }
     Column(
         modifier = modifier
     ) {
         OutlinedTextField(
             value = value,
             onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text(text = placeholder)
+            },
             leadingIcon = {
                 leadingIcon?.let {
                     Icon(
                         painter = painterResource(id = it),
-                        contentDescription = null
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
                     )
                 }
             },
-
+            trailingIcon = {
+                if (isPasswordField && value.isNotEmpty()) {
+                    val icon = if (passwordVisible) R.drawable.ic_visibility
+                    else R.drawable.ic_visibility_off
+                    Icon(
+                        painter = painterResource(id = icon),
+                        contentDescription = null,
+                        modifier = Modifier.clickable { passwordVisible = !passwordVisible },
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            },
+            visualTransformation = visualTransformation,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = imeAction,
+            ),
+            singleLine = true,
             shape = CircleShape,
             colors = OutlinedTextFieldDefaults.colors(
                 unfocusedContainerColor = MaterialTheme.colorScheme.surface,
                 focusedContainerColor = MaterialTheme.colorScheme.surface,
                 disabledContainerColor = MaterialTheme.colorScheme.surface,
                 focusedBorderColor = MaterialTheme.colorScheme.primary,
-                unfocusedBorderColor = if (errorMessage != null) {
-                    MaterialTheme.colorScheme.error
-                } else {
-                    MaterialTheme.colorScheme.onSurfaceVariant
-                },
+                unfocusedBorderColor = errorMessage?.let { MaterialTheme.colorScheme.error }
+                    ?: MaterialTheme.colorScheme.onSurfaceVariant
             )
         )
 
         errorMessage?.let {
             Text(
-                text = "Password Required",
-                modifier = Modifier
-                    .padding(start = 16.dp),
-                color = ColorError,
+                text = it,
+                modifier = Modifier.padding(start = 16.dp, top = 4.dp),
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.bodySmall
             )
         }
     }
@@ -84,8 +116,10 @@ private fun PrimaryTextFieldPreview() {
         PrimaryTextField(
             value = "",
             onValueChange = {},
+            placeholder = "Email",
             errorMessage = "Password Required",
             leadingIcon = R.drawable.ic_envelope,
+            keyboardType = KeyboardType.Password,
         )
     }
 }
