ee2bbb4d9eb4e3d3eae1b4e806d4afe6b5e2a25d
jhonatanneves <nevesjn2@gmail.com>
2025-12-08 10:44:26 -0300

    fix(LinkTalkNavigationState) Fixed the bug at the botton the buttomBar bug

--- app/src/main/java/com/example/linktalk/ui/feature/users/UsersViewModel.kt
+++ app/src/main/java/com/example/linktalk/ui/feature/users/UsersViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.linktalk.ui.feature.users
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.example.linktalk.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
@@ -10,5 +12,6 @@ class UsersViewModel @Inject constructor(
     userRepository: UserRepository
 ): ViewModel() {
     val usersFlow = userRepository.getUsers()
+        .cachedIn(viewModelScope) // *
 }
 
